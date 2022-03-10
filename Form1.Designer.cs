6d654f60a0bbc6ec319465d3fe73f63ad018fdf4
Prakash Mandal <pm1447616@gmail.com>
2022-03-10 20:44:46 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace db_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserBox = new System.Windows.Forms.ListBox();
+            this.PasswordBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PassBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            // 
+            // UserBox
+            // 
+            this.UserBox.FormattingEnabled = true;
+            this.UserBox.Location = new System.Drawing.Point(12, 29);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(184, 173);
+            this.UserBox.TabIndex = 1;
+            this.UserBox.SelectedIndexChanged += new System.EventHandler(this.UserBox_SelectedIndexChanged);
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.FormattingEnabled = true;
+            this.PasswordBox.Location = new System.Drawing.Point(210, 29);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(120, 173);
+            this.PasswordBox.TabIndex = 3;
+            this.PasswordBox.SelectedIndexChanged += new System.EventHandler(this.PasswordBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Passwords";
+            // 
+            // PassBox
+            // 
+            this.PassBox.FormattingEnabled = true;
+            this.PassBox.Location = new System.Drawing.Point(367, 29);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(160, 43);
+            this.PassBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox UserBox;
+        private System.Windows.Forms.ListBox PasswordBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox PassBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace db_test
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        string connecionString;
+        public Form1()
+        {
+            InitializeComponent();
+            connecionString = ConfigurationManager.ConnectionStrings["db_test.Properties.Settings.DBTestConnectionString"].ConnectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulaterUser();           
+        }
+        private void PopulaterUser()
+        {
+            using (connection = new SqlConnection(connecionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [User]", connection))
+            {
+                DataTable userTable = new DataTable();
+                adapter.Fill(userTable);
+
+                UserBox.DisplayMember = "Name";
+                UserBox.ValueMember = "Id";
+                UserBox.DataSource = userTable;
+            }                  
+        }
+        private void PopulatePasswords()
+        {
+            string query = "SELECT a.Name FROM Password a " +
+                "INNER JOIN UserPassword b ON a.Id = b.PasswordId " +
+                "WHERE b.UserId = @UserId";
+            using (connection = new SqlConnection(connecionString))             
+            using(SqlCommand command = new SqlCommand(query, connection))
+            using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@UserId",UserBox.SelectedValue);
+                DataTable Passwordtable = new DataTable();
+                adapter.Fill(Passwordtable);
+
+                PasswordBox.DisplayMember = "Name";
+                PasswordBox.ValueMember = "Id";
+                PasswordBox.DataSource = Passwordtable;
+            }
+        }   
+        
+        private void UserBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulatePasswords();
+        }
+
+        private void PasswordBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
